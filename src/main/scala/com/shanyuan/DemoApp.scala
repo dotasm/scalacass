d301c4249b8db26ea0e749132172945254246ef2
dotasm <ningyin75@hotmail.com>
2020-09-06 10:38:55 +0800

    add rabbitmq consumer

--- src/main/scala/com/shanyuan/DemoApp.scala
+++ src/main/scala/com/shanyuan/DemoApp.scala
@@ -3,9 +3,27 @@
  */
 package com.shanyuan
 
+import com.rabbitmq.client._
+
 object DemoApp extends App {
 
-	println("Hello Scala")
+	println("Hello RabbitMQ")
 
+  private val QUEUE_NAME = "hello"
+
+  
+  val factory = new ConnectionFactory()
+  factory.setHost("localhost")
+  val connection = factory.newConnection()
+  val channel = connection.createChannel()
+  channel.queueDeclare(QUEUE_NAME, false, false, false, null)
+  println("[*] Waiting for messages. To exit press CTRL+C")
+
+  val deliverCallback: DeliverCallback = (_, delivery) => {
+    val message = new String(delivery.getBody, "UTF-8")
+    println(" [x] Received '" + message + "'")
+  }
+
+  channel.basicConsume(QUEUE_NAME, true, deliverCallback, _ => {})
 }
 
